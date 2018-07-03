17b93873976953fc4143155d46f0937dd67b45af
Steren Giannini <steren.giannini@gmail.com>
2018-07-03 12:57:09 -0700

    Cleanup image after request

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 import os
+from io import BytesIO
 
 from flask import Flask
 from flask import send_file
 from flask import request
 from subprocess import call
 
+blender_file = 'script.blend'
+location = '/tmp/'
+suffix = 'tempfile'
+filename = location + suffix + '0001.png'
 
 app = Flask(__name__)
 
@@ -13,14 +18,18 @@ def render():
     name = request.args.get('name', default = 'Your name', type = str)
     app.logger.info("Received name: %s" % name)
 
-    expression = "import bpy; bpy.data.objects['Text'].data.body = '%s'" % name
-
-    location = '/tmp/'
-    suffix = 'tempfile'
-    blend_file = 'script.blend'
-    filename = location + suffix + '0001.png'
-    call('blender -b %s --python-expr "%s" -o %s%s -f 1' % (blend_file, expression, location, suffix), shell=True)
+    # This script changes the text, it is run inside Blender. 
+    blender_expression = "import bpy; bpy.data.objects['Text'].data.body = '%s'" % name
+    # Render 3D image
+    call('blender -b %s --python-expr "%s" -o %s%s -f 1' % (blender_file, blender_expression, location, suffix), shell=True)
+    
     return send_file(filename, mimetype='image/png')
+
+@app.after_request
+def cleanup(response):
+    os.remove(filename)
+    return response
+
 
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0',port=8080)
